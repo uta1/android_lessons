cffe08ddf8004eadf2cfea8236ea1829f54546e6
uta123 <uta123@yandex-team.ru>
2020-05-15 19:32:08 +0300

    add view model into fragment

--- sleep_tracker/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ sleep_tracker/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -24,23 +24,24 @@ import androidx.room.RoomDatabase
 @Database(entities = [SleepNight::class], version = 1,  exportSchema = false)
 abstract class SleepDatabase : RoomDatabase() {
     abstract val sleepDatabaseDao: SleepDatabaseDao
-    companion object {}
+    companion object {
 
-    @Volatile
-    private var INSTANCE: SleepDatabase? = null
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
 
-    fun getInstance(context: Context): SleepDatabase {
-        synchronized(this) {
-            var instance = INSTANCE
-            if (instance == null) {
-                instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        SleepDatabase::class.java,
-                        "sleep_history_database"
-                ).fallbackToDestructiveMigration().build()
-                INSTANCE = instance
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database"
+                    ).fallbackToDestructiveMigration().build()
+                    INSTANCE = instance
+                }
+                return instance
             }
-            return instance
         }
     }
 }
